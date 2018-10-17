2e148eb1147a39c00dab8ddf996e2147613dfd40
ohirangosta <syuji0116@gmail.com>
2018-10-17 10:35:34 +0900

    change detection method

--- similarity_of_slindingwindow_detect.py
+++ similarity_of_slindingwindow_detect.py
@@ -24,7 +24,7 @@ def function_E(Ra, Rn, Rt):
 	global C3
 	return Ra*C1-Rn*C2-Rt*C3
 
-def SimilarityBased_IntrusionDetect(Test_Data, k, div, WindowSize):
+def SimilarityBased_IntrusionDetect(Test_Data, k, div, WindowSize, similarity_set):
 	
 	# Ra is detection rate attack blocks 
 	# Rn is detection rate normal blocks
@@ -53,7 +53,6 @@ def SimilarityBased_IntrusionDetect(Test_Data, k, div, WindowSize):
 	with open(Test_Data) as Is:
 		# calculate Ra, Rn, Rt
 		for I in Is:
-
 			# create canid list of WindowSize
 			I_spt =	I.split(" ")
 			canpacket = I_spt[1].split("#")
@@ -75,7 +74,7 @@ def SimilarityBased_IntrusionDetect(Test_Data, k, div, WindowSize):
 				for canid in canid_list:
 					id_count[int(canid, 16)] += 1
 					if w_count != 1:
-						if id_count_prev[int(canid, 16)] != 0 and id_count[int(canid, 16)] != 0:
+						if similarity_set[int(canid, 16)] != 0 and id_count[int(canid, 16)] != 0:
 							num_intersection += 1
 				
 				# calculate Simpson coefficient
@@ -85,7 +84,7 @@ def SimilarityBased_IntrusionDetect(Test_Data, k, div, WindowSize):
 				# perform Intrusion Detection using Sliding Similarity
 				if SimpsonCoefficient < (ave-k*div) or (ave+k*div) < SimpsonCoefficient:
 					# True Positive
-					if True_count > False_count:
+					if True_count >= False_count:
 						Da += 1
 						Ra = (float(Da)/Ta)*100
 					# False Positive
@@ -98,7 +97,6 @@ def SimilarityBased_IntrusionDetect(Test_Data, k, div, WindowSize):
 				id_i = 0
 				True_count = 0
 				False_count = 0
-				id_count_prev = id_count
 	#print("W=%d, Ta=%d, Tn=%d, Da=%d, Dn=%d"%(WindowSize, Ta, Tn, Da, Dn))
 	return Ra, Rn, Rt
 
@@ -108,23 +106,58 @@ def SimulatedAnnealing_Optimize(DoS_Data, T=10000, cool=0.99):
 	k_best		= 0.8
 	div_best	= random.random()
 	W_best		= random.randint(5,70)
-	Ra, Rn, Rt 	= SimilarityBased_IntrusionDetect(DoS_Data, k_best, div_best, W_best)
+	#Ra, Rn, Rt 	= SimilarityBased_IntrusionDetect(DoS_Data, k_best, div_best, W_best)
+	Ra, Rn, Rt 	= 0, 0, 0
 	e_best		= function_E(Ra, Rn, Rt)
 	e_prev 		= function_E(Ra, Rn, Rt)-1
-	print("Ra=%f,Rn=%f,Rt=%f"%(Ra,Rn,Rt))
+
+	canid_list = list()
+	id_i = 0
+
+	#print("Ra=%f,Rn=%f,Rt=%f"%(Ra,Rn,Rt))
 	#print("E_best=%f"%e_best)
 
-	print("init Param:Deviation=%f, WindowSize=%d" %(div_best, W_best))
+	print("init_Param:Deviation=%f,WindowSize=%d" %(div_best, W_best))
 
 	while T > 0.0001 and e_prev < e_best:
 		# row 7 in paper
+		e_next_maxima = 0.0
+		W_count = 0
+		best_set = [0 for i in range(2048)]
+		temp_set = [0 for i in range(2048)]
+
 		div_next = random.random()
-		W_next = random.randint(W_best-10,W_best+10)
+		W_next = random.randint(W_best-10, W_best+10)
 
 		# row 8 in paper
-		Ra, Rn, Rt = SimilarityBased_IntrusionDetect(DoS_Data, k_best, div_next, W_next)
-		e_next = function_E(Ra, Rn, Rt)
-		print("Ra=%f,Rn=%f,Rt=%f"%(Ra,Rn,Rt))
+		with open(DoS_Data) as Is:
+			for best_window in range(0, int(Tn_packet/W_next)):
+				for I in Is:
+					I_spt =	I.split(" ")
+					canpacket = I_spt[1].split("#")
+					canid_list.append(canpacket[0])
+
+					id_i += 1
+
+					if id_i == W_next:
+						for canid in canid_list:
+							# 類似度を比較する集合を算出
+							temp_set[int(canid, 16)] = 1
+						if best_window == W_count:
+							Ra, Rn, Rt = SimilarityBased_IntrusionDetect(DoS_Data, k_best, div_next, W_next, temp_set)
+							e_next = function_E(Ra, Rn, Rt)
+							if e_next_maxima < e_next:
+								e_next_maxima = e_next
+								best_set = temp_set
+						W_count += 1
+						id_i = 0
+						canid_list = list()
+						temp_set = [0 for i in range(2048)]
+		for canid in range(0, 2048):
+			if best_set[canid] == 1:
+				print("[WindowSize=%d], %x"%(W_next, canid))
+		e_next = e_next_maxima
+		print("Ra=%f,Rn=%f,Rt=%f,Precision=%f"%(Ra,Rn,Rt,float(Ra)/(Ra+Rn)))
 		#print("E=%f,E_best=%f"%(e_next,e_best))
 
 		# calcurate probability from templature.
